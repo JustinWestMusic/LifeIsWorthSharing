a13711c010eba0a7815bd68b3ad7f7ae2114c97c
JustinWestMusic <j_desjardins-west@fanshaweonline.ca>
2017-03-27 14:51:52 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,29 +4,6 @@ $(document).foundation();
 {
 	"use strict";
 
-	//---SMOOTH SCROLL SECTION---
-	
-	//Variables
-
-	var topMenu = document.querySelector("#top-menu");
-	var navLinks = topMenu.querySelectorAll("a");
-
-	for (var i = 0; i < navLinks.length; i++)
-		{
-			navLinks[i].addEventListener("click", scrollto, false);
-		}
-	
-	//Function
-	
-	function scrollto(e)
-	{
-		//console.log("click");
-		var idNum = e.currentTarget.id;
-		console.log(idNum);
-		TweenMax.to(window, 1,{scrollTo:{y:"#lifeSec"+idNum, offsetY:70}});
-	}
-
-
 	//---FADE IN SECTION ELEMENTS---
 
 	//Variables
